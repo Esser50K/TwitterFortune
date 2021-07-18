18b3b8a860f1dbaff342c9f37a55cf3df93592e5
André Esser <bepandre@hotmail.com>
2021-07-18 22:08:29 +0100

    enables adding static hashtags and adjusting random time intervals

--- fortunebot.py
+++ fortunebot.py
@@ -7,18 +7,36 @@ COMMAND = "fortune"
 if os.getenv("OFFENSIVE", None):
     COMMAND += " -o"
 
-MIN_TIME_BETQEEN_TWEETS = 60 * 60 * 6 # 6 Hours
-MAX_TIME_BETQEEN_TWEETS = 60 * 60 * 12 # 12 Hours
+HASHTAGS = os.getenv("HASHTAGS", "").split(",")
+MAX_TAGS = int(os.getenv("MAX_TAGS", 3))
+
+MIN_TIME_BETWEEN_TWEETS = 60 * 60 * 6  # 6 Hours
+MAX_TIME_BETWEEN_TWEETS = 60 * 60 * 12  # 12 Hours
+
+try:
+    MIN_TIME_BETWEEN_TWEETS = int(os.getenv("MIN_TIME_BETWEEN_TWEETS", None))
+except: pass
+
+try:
+    MAX_TIME_BETWEEN_TWEETS = int(os.getenv("MAX_TIME_BETWEEN_TWEETS", None))
+except: pass
+
 
 auth = tweepy.OAuthHandler(os.getenv("TWITTER_API_KEY"), os.getenv("TWITTER_SECRET_KEY"))
 auth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
 api = tweepy.API(auth)
 
-while(True):
+
+while True:
+    random.shuffle(HASHTAGS)
+    hashtags = HASHTAGS
+    if len(hashtags) > MAX_TAGS:
+        hashtags = hashtags[:MAX_TAGS]
+
+    hashtags = "\n" + " ".join(map(lambda x: "#"+x, hashtags))
     fortune = ""
     while fortune == "" or len(fortune) > 280:
-
-        fortune = os.popen(COMMAND).read()
+        fortune = os.popen(COMMAND).read()+hashtags
         time.sleep(1)
 
     try:
@@ -27,5 +45,5 @@ while(True):
     except Exception as e:
         print("failed to tweet fortune with len %d: %s" % (len(fortune), fortune))
 
-    time.sleep(random.randint(MIN_TIME_BETQEEN_TWEETS, MAX_TIME_BETQEEN_TWEETS))
+    time.sleep(random.randint(MIN_TIME_BETWEEN_TWEETS, MAX_TIME_BETWEEN_TWEETS))
 
